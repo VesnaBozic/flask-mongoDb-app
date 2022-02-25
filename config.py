9f3bd202779ac9d2f2d3f4fa7de2a9dee6af801e
Vesna Bozic <61289765+VesnaBozic@users.noreply.github.com>
2022-02-25 02:35:24 +0100

    .

---
+++ config.py
@@ -0,0 +1,10 @@
+#this is configuration module to store some major configurations that
+# we use across the application/
+
+import os
+
+#we are going to put all our configurations in a class
+# and then we can access all these properties throught this class Config
+class Config(object):
+    SECRET_KY = os.environ.get('SECRET_KEY') or "secret_string"
+    #SECERET KEY is a special key that is used as signature key to make sure that anything we sent across the server is not being altered
