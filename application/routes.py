c13135599bfd7ae3d839c11a6456d90b28517668
Vesna Bozic <61289765+VesnaBozic@users.noreply.github.com>
2022-02-27 13:31:44 +0100

    .

--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,14 @@
 from application import app, db
-from flask import render_template, request, json, Response, redirect,flash, url_for
-from application.models import User,Course, Enrollment
+from flask import render_template, request, json, Response, redirect,flash, url_for, session
+from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 
 # first I used data from list of dictionaries, but later switch to database, this won't be used anymore
-classes = [ {"courseID":"1111","title":"PY 101","description":"Intro to Python","credits":3,"term":"Fall, Spring"},
-                {"courseID":"2222","title":"Flask 1","description":"Intro to Flask","credits":4,"term":"Spring"}, 
-                {"courseID":"3333","title":"Int Py 201","description":"Intermediate Python Programming","credits":3,"term":"Fall"},
-                {"courseID":"4444","title":"Adv F","description":"Advanced Flask","credits":3,"term":"Fall, Spring"}, 
-                {"courseID":"5555","title":"FS","description":"Flask Security","credits":4,"term":"Fall"}]
+# classes = [ {"courseID":"1111","title":"PY 101","description":"Intro to Python","credits":3,"term":"Fall, Spring"},
+#                 {"courseID":"2222","title":"Flask 1","description":"Intro to Flask","credits":4,"term":"Spring"}, 
+#                 {"courseID":"3333","title":"Int Py 201","description":"Intermediate Python Programming","credits":3,"term":"Fall"},
+#                 {"courseID":"4444","title":"Adv F","description":"Advanced Flask","credits":3,"term":"Fall, Spring"}, 
+#                 {"courseID":"5555","title":"FS","description":"Flask Security","credits":4,"term":"Fall"}]
 
 @app.route("/")
 @app.route("/index")
@@ -19,11 +19,16 @@ def index():
 
 @app.route("/courses")
 @app.route("/courses/<term>")
-def courses(term="Spring 2022"):
+def courses(term=None):
+    if term is None:
+        term = "Spring 2022"
+    classes = Course.objects.order_by("+courseID") #with order_by we are sorting courses + asc order, - desc, default asc 
     return render_template("courses.html",  classes = classes, course = True, term = term)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if session.get('username'):
+        return redirect(url_for('index'))
     registration = RegisterForm()
     if registration.validate_on_submit():
         user_id    =  User.objects.count() #this count number of objects in database
@@ -46,6 +51,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if session.get('username'):
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
         email    =   form.email.data
@@ -57,24 +64,77 @@ def login():
             #flashing system is Flask way to give feedback to a user
             #flashing with categories we use to display success or danger message
             flash(f"{user.first_name}, You are logged in!", "success")
+            session['user_id'] = user.user_id
+            session['username'] = user.first_name
             return redirect("/index")
         else:
             flash("Something went wrong! Try again!", "danger")
     return render_template("login.html",title = "Login", form = form, login=True)
 
+@app.route("/logout")
+def logout():
+    #ending session
+    session['user_id'] = False
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
 @app.route("/enrollment", methods =["GET", "POST"])
 def enrollment():
-    #THIS IS FOR GET METHOD
-    # id = request.args.get('courseID')
-    # title = request.args.get('title')
-    # term = request.args.get('term')
 
-    #THIS IS FOR POST
-    id = request.form.get('courseID')
-    title = request.form.get('title')
+    if session.get('username'):
+        return redirect(url_for('login'))
+     
+    courseID = request.form.get('courseID')
+    course_title = request.form.get('title')
+   
+    user_id = session.get('user_id')
+
+    if courseID:
+        if Enrollment.objects(user_id=user_id,courseID=courseID):
+            flash(f"You are already enrolled in the {course_title}!", "danger")
+            return redirect(url_for("courses"))
+        else:
+            Enrollment(user_id=user_id,courseID=courseID).save()
+            flash(f"You are now enrolled in the {course_title}!", "success")
+
+    classes = list(User.objects.aggregate(*[
+            {
+                '$lookup': {
+                    'from': 'enrollment', 
+                    'localField': 'user_id', 
+                    'foreignField': 'user_id', 
+                    'as': 'r1'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$r1', 
+                    'includeArrayIndex': 'r1_id', 
+                    'preserveNullAndEmptyArrays': False
+                }
+            }, {
+                '$lookup': {
+                    'from': 'course', 
+                    'localField': 'r1.courseID', 
+                    'foreignField': 'courseID', 
+                    'as': 'r2'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$r2', 
+                    'preserveNullAndEmptyArrays': False
+                }
+            }, {
+                '$match': {
+                    'user_id': user_id
+                }
+            }, {
+                '$sort': {
+                    'courseID': 1
+                }
+            }
+]))
     term = request.form.get('term')
-
-    return render_template("enrollment.html", enrollment=True, course = {"id": id, "title":title, "term" : term})
+    return render_template("enrollment.html", title="Enrollment", enrollment=True, classes=classes)
 
 @app.route("/api/")
 @app.route("/api/<index>")
